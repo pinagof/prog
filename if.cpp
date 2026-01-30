1933e6b096704db7de0dfc18371e54f2af5bc143
pinagof <pinagof@ieselaios.com>
2026-01-30 09:54:57 +0000

    update

---
+++ if.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int p1(void) {
+     int i;
+     i=0;
+     while (i<6)
+     {
+         cout << "Hello, Fran!" << endl;
+         i++;
+     }
+    int e;
+    e=0;
+    while (e<1299)
+    {
+        cout << "Eres un maquina, Fran!" << endl;
+        e++;
+    }
+}
+int p2(void) {
+    int a;
+    a=0;
+    while (a<728)
+    {
+        cout << "Podrias ayudar mas en casa" << endl;
+        a++;
+    } 
+}
+int p3(void) {
+    int o;
+    o=0;
+    while (o<15000)
+    {
+        cout << "Eres un maquina, una fiera, un mastodonte" << endl;
+        o++;
+    }
+} 
+
+if (condition)
+{
+    /* code */
+}
