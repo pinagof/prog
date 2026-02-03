58f7bd2d61a3b8f9ed19fdd9a8e24a11d249bf7b
pinagof <pinagof@ieselaios.com>
2026-02-03 09:00:31 +0000

    update ej5

---
+++ ej5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int n;
+        cout << "Cuantos numeros me vas a dar\n";
+        cin >> n;
+        int valor[n];
+    
+        int 1;
+        i=0;
+        while (i< n){
+            cout << "Dame el numero "<< i <<"\n";
+            cin >> sumar[i];
+             i++;
+        }
+        int j;
+        j=0;
+        while(j<n){
+            cout << "<<valor"<<j<<"es"<<valor[j]<<"\n";
+            j++;
+        } 
+        }
+        
