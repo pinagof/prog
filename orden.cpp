40774e358c6b5d81067e5d4f7c25ee4a4a747f80
pinagof <pinagof@ieselaios.com>
2026-02-13 09:35:53 +0000

    update ordena

---
+++ orden.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+defining int float
+using namespace std;
+int main(){
+   int n;
+    cout << "Cuantos numeros me vas a dar y quieres ordenar\n";
+    cin >> n;
+    int vl[n];
+    
+    for (int i=0; i<n; i++){
+        cout << "Dame el numero "<< i <<"\n";
+            cin >> vl[i];
+    }
+    int menor, psmenor, mayor, psmayor;
+
+    for (int i=0; i<n; i++){
+        menor=vl[i];psmenor=i;
+        for (int j=i+1; j<n; j++){
+            if (vl[j]<menor){
+                menor=vl[j]; psmenor=j;
+            }
+        }
+    }
+    int temp;
+    vl[i]=menor;
+    vl[psmenor]=temp;
+    }
+    cout << "Los numeros ordenados son:\n";   
